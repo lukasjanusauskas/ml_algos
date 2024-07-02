1ba73260a013e55e21f18d58e9e5c39e955aa51d
lukasjanusauskas <lukasjanusauskaslj@gmail.com>
2024-07-02 23:13:16 +0300

    Added file selection

--- decision_trees/main.cpp
+++ decision_trees/main.cpp
@@ -5,17 +5,56 @@
 
 rapidcsv::Document get_user_dataset();
 void print_columns(rapidcsv::Document df);
+std::vector<std::string> get_datasets();
+void print_datasets();
+std::vector<int> get_columns(rapidcsv::Document df);
 
 int main(){
     rapidcsv::Document df = get_user_dataset();
 
+    std::cout << "Available columns:" << "\n";
+    for (std::string s: df.GetColumnNames())
+        std::cout << s << " ";
+
+    std::vector<int> train_columns = get_columns(df);
+
     return 0;
 }
 
-rapidcsv::Document get_user_dataset(){
-    // Print possible datasets
-    std::cout << "Possible datasets:";
-    system("ls data");
+void print_datasets(){
+    std::cout << "Possible datasets:\n";
+    int code = system("ls data");
 
-    return rapidcsv::Document();
+    if(code != 0) exit(EXIT_FAILURE);
+}
+
+std::vector<std::string> get_datasets(){
+    int code = system("ls data >> tmp.txt");
+
+    if(code != 0) exit(EXIT_FAILURE);
+
+    std::ifstream fr("tmp.txt");
+    std::vector<std::string> files; 
+
+    for(std::string s; std::getline(fr, s);)
+        files.push_back(s);
+
+    fr.close(); system("rm tmp.txt");
+
+    return files;
+}
+
+rapidcsv::Document get_user_dataset(){
+    print_datasets();
+    std::vector<std::string> files = get_datasets();
+
+    int index = INT16_MAX;
+    while(index >= files.size()){
+        std::cout << "Choose, which will be used for the decision tree" << std::endl;
+        std::cin >> index;
+    }
+
+    std::string path = std::string("data/") + files.at(index);
+
+    return rapidcsv::Document(path);
 }
